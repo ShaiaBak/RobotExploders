8f6e86f8cf903ce42a2fb58d650c2d5dbb6f8225
slushee <tylerpham.9@gmail.com>
2014-06-18 14:37:45 -0700

    Basic 2D control and Camera Tracking

---
+++ Build 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour 
+{
+	public Transform player;
+	public Vector2 margin, smoothing;
+	public BoxCollider2D bounds;
+	private Vector3 min, max;
+	public bool isFollowing { get; set; } 
+
+	public void Start()
+	{
+		min = bounds.bounds.min;
+		max = bounds.bounds.max;
+		isFollowing = true;
+	}
+	public void Update()
+	{
+		var x = transform.position.x;
+		var y = transform.position.y;
+
+		if (isFollowing)
+		{
+			if (Mathf.Abs( x - player.position.x ) > margin.x)
+				x = Mathf.Lerp (x, player.position.x, smoothing.x * Time.deltaTime);
+
+			if (Mathf.Abs( y - player.position.y ) > margin.y)
+				y = Mathf.Lerp (y, player.position.y, smoothing.y * Time.deltaTime);
+		}
+
+		var cameraHalfWdith = camera.orthographicSize * ((float) Screen.width / Screen.height);
+
+		x = Mathf.Clamp(x, min.x + cameraHalfWdith/2, max.x - cameraHalfWdith/2);
+		y = Mathf.Clamp(y, min.y + camera.orthographicSize, max.y - camera.orthographicSize);
+		transform.position = new Vector3(x, y, transform.position.z);
+
+	}
+}
---
+++ Build 1/Assets/Scripts/Controller1.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller1 : MonoBehaviour {
+
+
+	public float maxSpeed = 3f; 	//Arbitrary speed value
+	private bool facingRight = true;
+
+
+	public float jumpForce = 50;	//Arbitrary jump value
+	public bool enableControl = true;
+	//private Animator anim;
+	private float moveH = 0f;
+	private float moveV = 0f;
+	public bool grounded = false;
+	public bool jump;
+	public bool doubleJump = false;
+	public Transform groundCheck;
+	private float groundRadius = 0.1f;
+	public LayerMask whatIsGround;
+
+
+	void Start () {
+		//anim = GetComponent<Animator>();
+	}
+	
+	void FixedUpdate () {
+		//Only allow jumping when player is on the ground
+
+		if ((grounded || !doubleJump) && jump ) {
+			//anim.SetBool ("Ground", false);
+			rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+			if (!grounded && !doubleJump) {
+				doubleJump = true;
+			}
+		}
+
+		if (enableControl) {
+			//This checks the object "groundCheck", gives it a radius of "groundRadius"
+			//If the "groundCheck" overlaps with anything that is tagged "whatIsGround"
+			//the unit will be considered on the ground, grounded = true
+			grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+			//anim.SetBool ("Ground", grounded);
+
+			if (grounded) {
+				doubleJump = false;
+			}
+
+			//When the vertical speed is not zero, change to the jumping/falling animation
+			//anim.SetFloat ("speedV", rigidbody2D.velocity.y);
+
+			//When the horizontal speed is not zero, change to the walking animation
+			//anim.SetFloat ("speedH", Mathf.Abs (moveH));
+			//Handles horizontal speed and orientation
+			rigidbody2D.velocity = new Vector2 (moveH * maxSpeed, rigidbody2D.velocity.y);
+			if (moveH > 0 && !facingRight) {
+					Flip ();
+			} else if (moveH < 0 && facingRight) {
+					Flip ();
+			}
+		} else {
+			moveH = 0f; 
+		}
+
+
+	}
+	void Update () {
+		//Player Inputs
+		moveH = Input.GetAxis ("P1_Horizontal");
+		moveV = Input.GetAxis ("P1_Vertical");
+		jump = Input.GetButton ("P1_Jump");
+
+
+
+	}
+
+
+	void Flip () {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
