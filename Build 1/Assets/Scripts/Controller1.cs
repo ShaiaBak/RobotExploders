b0372e0d04d26892d4c0e21f4e44ae3eb3e82af8
slushee <tylerpham.9@gmail.com>
2014-06-21 17:24:51 -0700

    Flight for player 1

--- Build 1/Assets/Scripts/Controller1.cs
+++ Build 1/Assets/Scripts/Controller1.cs
@@ -4,18 +4,25 @@ using System.Collections;
 public class Controller1 : MonoBehaviour {
 
 
-	public float maxSpeed = 3f; 	//Arbitrary speed value
+	public float maxSpeed = 3f; 			//Arbitrary speed value
 	private bool facingRight = true;
 
 
-	public float jumpForce = 250;	//Arbitrary jump value
+	public float jumpForce = 250;			//Arbitrary jump value
 	public bool enableControl = true;
 	//private Animator anim;
+
+	//--------Input Variables--------//
 	private float moveH = 0f;
 	private float moveV = 0f;
-	public bool grounded = false;
-	public bool jump;
-	public bool doubleJump = true;
+	public bool jumpPress;					//When the Jump button is pressed
+	public bool jumpRelease;				//When the Jump button is released
+
+	public bool grounded = false;			//checks if object on the ground
+	public bool doubleJump = true;			//True = Doublejump is available
+	public bool flyingMode = false;			//True = Flying is active
+	public float flyingModeTimer = 0;		//The Timer for flying mode
+	public float flyingModeDuration = 2;	//Total Duration of the flight
 	public Transform groundCheck;
 	private float groundRadius = 0.1f;
 	public LayerMask whatIsGround;
@@ -26,9 +33,6 @@ public class Controller1 : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
-		//Only allow jumping when player is on the ground
-
-
 
 		if (enableControl) {
 			//This checks the object "groundCheck", gives it a radius of "groundRadius"
@@ -36,24 +40,38 @@ public class Controller1 : MonoBehaviour {
 			//the unit will be considered on the ground, grounded = true
 			grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 			//anim.SetBool ("Ground", grounded);
-			
+
+			//When player is on the ground, second jump is available
 			if (grounded) {
 				doubleJump = true;
-				if (jump) {
+				//When jump button is pressed, add a force upwards
+				if (jumpPress) {
 					rigidbody2D.AddForce (new Vector2 (0, jumpForce));
 				}
-				
-			} else if (doubleJump && jump) {
+				//if second jump is available and the jump button is pressed
+				//enter flying mode, turn off gravity and reset the timer
+			} else if (doubleJump && jumpPress) {
 				doubleJump = false;
-				rigidbody2D.AddForce (new Vector2 (0, jumpForce));
+				flyingMode = true;
+				rigidbody2D.gravityScale = 0;
+				flyingModeTimer = flyingModeDuration;
 			}
 			//When the vertical speed is not zero, change to the jumping/falling animation
 			//anim.SetFloat ("speedV", rigidbody2D.velocity.y);
 
 			//When the horizontal speed is not zero, change to the walking animation
 			//anim.SetFloat ("speedH", Mathf.Abs (moveH));
-			//Handles horizontal speed and orientation
-			rigidbody2D.velocity = new Vector2 (moveH * maxSpeed, rigidbody2D.velocity.y);
+
+
+
+			// If the player has activated "Flying Mode", the player can move freely in the X and Y
+			// If not, the player can only move in the X
+			if (flyingMode){
+				rigidbody2D.velocity = new Vector2 (moveH * maxSpeed, moveV * maxSpeed);
+			} else {
+				rigidbody2D.velocity = new Vector2 (moveH * maxSpeed, rigidbody2D.velocity.y);
+			}
+			// Flip the image if it is moving to left while facing right or moving right while facing left
 			if (moveH > 0 && !facingRight) {
 					Flip ();
 			} else if (moveH < 0 && facingRight) {
@@ -70,8 +88,20 @@ public class Controller1 : MonoBehaviour {
 		//Player Inputs
 		moveH = Input.GetAxis ("P1_Horizontal");
 		moveV = Input.GetAxis ("P1_Vertical");
-		jump = Input.GetButtonDown ("P1_Jump");
+		jumpPress = Input.GetButtonDown ("P1_Jump");
+		jumpRelease = Input.GetButtonUp ("P1_Jump");
 
+
+		//When flying, reduce timer by the time it took to complete the last frame
+		if (flyingMode) {
+			flyingModeTimer -= Time.deltaTime;
+		}
+		//When timer is equal to or less than zero or when the jump button is released
+		//Disable flying mode and restore the gravity setting
+		if (flyingModeTimer <= 0 || jumpRelease) {
+			flyingMode = false;
+			rigidbody2D.gravityScale = 1;
+		}
 	}
 
 
