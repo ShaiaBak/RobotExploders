4f1b43f5528fe4555905c830456c740dfee54dc6
Shaia <shaia.bak@hotmail.com>
2014-07-20 13:50:30 -0700

    controller assignment bug fix

--- Build 1/Assets/Scripts/inputManager.cs
+++ Build 1/Assets/Scripts/inputManager.cs
@@ -7,7 +7,7 @@ public class inputManager : MonoBehaviour {
 	public bool inputSwitch_p1;				// when input switch for player 1 is pressed
 	public bool inputSwitch_p2;				// when input switch for player 2 is pressed
 	public Controller1 p1;
-	public Controller2 p2;
+	public Controller2p1 p2;
 
 	// Use this for initialization
 	void Start () {
@@ -24,14 +24,16 @@ public class inputManager : MonoBehaviour {
 		if (inputSwitch_p1) {
 			// when one control scheme is on, turn other off
 			Destroy(p2);
+			Destroy(p1);
 			// add p1 control scheme
 			p1 = gameObject.AddComponent("Controller1") as Controller1;
 		}
 		if (inputSwitch_p2) {
 			// when one control scheme is on, turn other off
 			Destroy(p1);
+			Destroy(p2);
 			// add p2 control scheme
-			p2 = gameObject.AddComponent("Controller2") as Controller2;
+			p2 = gameObject.AddComponent("Controller2p1") as Controller2p1;
 
 		}
 	}
