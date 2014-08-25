6e69bee4b3954d628398e9078b6cbef6d4ea1237
Shaia <shaia.bak@hotmail.com>
2014-08-24 18:06:56 -0700

    OLD MAN YELLS AT CLOUD

---
+++ build-2/Assets/Scripts/CloudController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudController : MonoBehaviour {
+	public bool left;
+	public bool right;
+
+	// Use this for initialization
+	void Start () {
+		transform.Translate(Vector3.forward * Input.GetAxis("Horizontal") * Time.deltaTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+	}
+}
