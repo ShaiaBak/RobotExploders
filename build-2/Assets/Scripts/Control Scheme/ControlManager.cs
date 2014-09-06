c7082729c2642daf586af49e3842f92599d33678
Howie Chan <hw.chan@hotmail.com>
2014-09-06 13:28:34 -0700

    Player controls are flipped depending on 7/8 pressed

--- build-2/Assets/Scripts/Control Scheme/ControlManager.cs
+++ build-2/Assets/Scripts/Control Scheme/ControlManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// TODO: Recode this completely when we have a controls selection screen
 public class ControlManager : MonoBehaviour {
 	
 	public ControlScheme P1Controls;
@@ -11,27 +12,39 @@ public class ControlManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		// Give Player1 P1 keyboard controls
+		// Give Player1 P1 keyboard controls & vice versa
 		if(Input.GetButtonDown("p1_control1")){
 			print ("Player1 now has P1 keyboard controls");
+			print ("Player2 now has P2 keyboard controls");
 			Destroy(P1.GetComponent<ControlScheme>());
+			Destroy(P2.GetComponent<ControlScheme>());
 			// Set the controls
-			KeyboardScheme ks = P1.AddComponent<KeyboardScheme>();
-			ks.SetPlayerNumberControlScheme(1);
-			P1Controls = ks;
+			KeyboardScheme ks1 = P1.AddComponent<KeyboardScheme>();
+			KeyboardScheme ks2 = P2.AddComponent<KeyboardScheme>();
+			ks1.SetPlayerNumberControlScheme(1);
+			ks2.SetPlayerNumberControlScheme(2);
+			P1Controls = ks1;
+			P2Controls = ks2;
 			// Set the reference for pilot
-			P1.GetComponent<Pilot>().controls = ks;
+			P1.GetComponent<Pilot>().controls = ks1;
+			P2.GetComponent<Pilot>().controls = ks2;
 		}
-		// Give Player1 P2 keyboard controls
+		// Give Player1 P2 keyboard controls & vice versa
 		else if(Input.GetButtonDown("p1_control2")){
 			print ("Player1 now has P2 keyboard controls");
+			print ("Player2 now has P1 keyboard controls");
 			Destroy(P1.GetComponent<ControlScheme>());
+			Destroy(P2.GetComponent<ControlScheme>());
 			// Set the controls
-			KeyboardScheme ks = P1.AddComponent<KeyboardScheme>();
-			ks.SetPlayerNumberControlScheme(2);
-			P1Controls = ks;
+			KeyboardScheme ks1 = P1.AddComponent<KeyboardScheme>();
+			KeyboardScheme ks2 = P2.AddComponent<KeyboardScheme>();
+			ks1.SetPlayerNumberControlScheme(2);
+			ks2.SetPlayerNumberControlScheme(1);
+			P1Controls = ks1;
+			P2Controls = ks2;
 			// Set the reference for pilot
-			P1.GetComponent<Pilot>().controls = ks;
+			P1.GetComponent<Pilot>().controls = ks1;
+			P2.GetComponent<Pilot>().controls = ks2;
 		}
 	}
 }
--- build-2/Assets/Scripts/GUIManager.cs
+++ build-2/Assets/Scripts/GUIManager.cs
@@ -4,32 +4,39 @@ using System.Collections;
 public class GUIManager : MonoBehaviour {
 
 	public PlayerHealthSystem ph;
-	public Controller1 pc;
+	private Pilot ps;
 	private Transform player;
 
 	void Start(){
 		// Grab the player defined in the Camera Controller component
 		player = GetComponent<CameraController>().player;
 		ph = player.GetComponent<PlayerHealthSystem>();
+		ps = player.GetComponent<Pilot>();
 	}
 
 	void OnGUI(){
-		// TODO: We shouldn't check for player 1 by checking its name
-		if(player.name == "P1"){
-
+		// Player 1
+		if(ps.isP1){
 			// Show pilot hp
 			GUI.Label(new Rect(10,10,100,20), ph.curHP.ToString() + "/" + ph.maxHP.ToString());
 
 			// Check whether to show golem hp
-			// TODO: We shouldn't check whether the player has controller1 in OnGUI
-			pc = player.GetComponent<Controller1>();
-			if(pc != null && pc.inGolem){
-				GolemHealthSystem gh = pc.golem.GetComponent<GolemHealthSystem>();
+			if(ps.currentGolem != null){
+				GolemHealthSystem gh = ps.currentGolem.GetComponent<GolemHealthSystem>();
 				GUI.Label(new Rect(10,30,100,20), gh.curHP.ToString() + "/" + gh.maxHP.ToString());
 			}
 
-		}else{
+		}
+		// Player 2
+		else{
+			// Show pilot hp
 			GUI.Label(new Rect(Screen.width/2+20,10,100,20), ph.curHP.ToString() + "/" + ph.maxHP.ToString());
+
+			// Check whether to show golem hp
+			if(ps.currentGolem != null){
+				GolemHealthSystem gh = ps.currentGolem.GetComponent<GolemHealthSystem>();
+				GUI.Label(new Rect(Screen.width/2+20,30,100,20), gh.curHP.ToString() + "/" + gh.maxHP.ToString());
+			}
 		}
 	}
 }
