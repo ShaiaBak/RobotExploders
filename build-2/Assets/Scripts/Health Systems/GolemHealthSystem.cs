4490d7542a8e39dce956640badac551684a70d39
slushee <tylerpham.9@gmail.com>
2014-09-07 16:16:21 -0700

    Pilot escape time

--- build-2/Assets/Scripts/Health Systems/GolemHealthSystem.cs
+++ build-2/Assets/Scripts/Health Systems/GolemHealthSystem.cs
@@ -4,15 +4,21 @@ using System.Collections;
 // TODO: :) Everything.
 public class GolemHealthSystem : HealthSystem {
 	private GameController gameController;
-	
+	public int escapeTime = 2;
+
 	protected override void HandleDeath(){
+		GetComponent<Golem>().enableControl = false;
+		StartCoroutine(Wait());
+	}
+	IEnumerator Wait() {
+		yield return new WaitForSeconds(escapeTime);
 
 		gameController = GameObject.Find("GameController").GetComponent<GameController>();
 		gameController.GameOver();
-
+		
 		renderer.material.color = Color.gray;
 		collider2D.enabled = false;
-
+		
 		tag = "Dead";
 	}
 }
