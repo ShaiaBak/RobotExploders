4ce195fe8e9a018a6e5181847014d1aff33f3e3a
slushee <tylerpham.9@gmail.com>
2014-09-14 16:55:38 -0700

    Birds and Scarves

--- build-2/Assets/Scripts/BirdReaction.cs
+++ build-2/Assets/Scripts/BirdReaction.cs
@@ -5,21 +5,22 @@ public class BirdReaction : MonoBehaviour {
 
 	public float hitRadius = 0.5f;
 	public float speedH =  2.5f;
-	public float speedV = 1.75f;
+	public float speedV = 2.0f;
 	public bool isShoo = false;
 	public bool isFlying = false;
 	public float flyingTimerDuration = 2f;
 	private float flyingTimer = 0f;
-	public bool atPeak = false;
+	private bool atPeak = false;
 	private float counter = 0f;
-	public float hoverScale = 1f;
+	private float hoverScaleY = 1f;
+	private float hoverScaleX = 1f;
 	//public LayerMask scaresBirds;
-	public Vector2 difference;
-	public float atPeakX;
+	private Vector2 difference;
+	private float atPeakX;
 	
 	void Start () {
-	
-		hoverScale = Random.Range(0.01f, 0.020f);
+		hoverScaleX = Random.Range(1.25f, 1.75f);
+		hoverScaleY = Random.Range(0.01f, 0.02f);
 		flyingTimer = flyingTimerDuration;
 	}
 	
@@ -40,7 +41,7 @@ public class BirdReaction : MonoBehaviour {
 		if (atPeak) {
 			counter += Time.deltaTime/2;
 //			Debug.Log(atPeakX);
-			transform.position = new Vector2 ( atPeakX + Mathf.Sin(counter)*10f , transform.position.y + Mathf.Sin(counter)*hoverScale );
+			transform.position = new Vector2 ( atPeakX + Mathf.Sin(counter)*10f*hoverScaleX, transform.position.y + Mathf.Sin(counter)*hoverScaleY );
 			//rigidbody2D.velocity = new Vector2 (0f, Mathf.Sin(Time.time)*hoverScale);
 			//transform.localPosition.x += 1f;
 		}
@@ -50,7 +51,7 @@ public class BirdReaction : MonoBehaviour {
 		if (other.collider2D.tag == "Player" || other.collider2D.tag == "Golem") {
 		difference = other.transform.position - transform.position;
 		if (difference.x < 0f && !isShoo) {
-			speedH =  Random.Range (2.5f,3.5f);
+			speedH =  Random.Range (3.0f,3.5f);
 			rigidbody2D.velocity = new Vector2 (speedH, speedV);
 			isShoo = true; 	
 			isFlying = true;
@@ -58,7 +59,7 @@ public class BirdReaction : MonoBehaviour {
 		}
 
 		if (difference.x > 0f && !isShoo) {
-			speedH =  Random.Range (2.5f,3.5f);
+			speedH =  Random.Range (3.0f,3.5f);
 			rigidbody2D.velocity = new Vector2 (-speedH, speedV);	
 			isShoo = true; 
 			isFlying = true;
