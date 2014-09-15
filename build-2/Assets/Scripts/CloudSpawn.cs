cbf23261e4d71c96cafa801b2164aa04813aad52
Shaia <shaia.bak@hotmail.com>
2014-09-14 17:09:56 -0700

    Cloud Skyhook

--- build-2/Assets/Scripts/CloudSpawn.cs
+++ build-2/Assets/Scripts/CloudSpawn.cs
@@ -5,36 +5,32 @@ public class CloudSpawn : MonoBehaviour {
 	public GameObject Cloud;
 	public Vector2 spawnValues;
 	public int cloudCount = 10;
-	public bool right;
-	public bool left;
 	public float cloudWait;
 
+	// on start cloud variables
+	public Vector2 spawnValuesBegin;
+	public int cloudCountBegin = 10;
+
 	void Start()
-	{ 
+	{
 		StartCoroutine ("cloudSpawnLeft");
+		StartCoroutine ("cloudSpawnBegin");
 		//cloudSpawnLeft ();
 		//cloudSpawnRight ();
 	}
 	IEnumerator cloudSpawnLeft(){
-			Debug.Log ("lol");
 			for(int i = 0; i < cloudCount; i++){
-				Vector2 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), Random.Range (-spawnValues.y, spawnValues.y));
+				Vector2 spawnPosition = new Vector2 (Random.Range(-spawnValues.x, spawnValues.x) + transform.position.x, Random.Range (-spawnValues.y, spawnValues.y) + transform.position.y);
 				GameObject CloudLeft = (GameObject) Instantiate (Cloud, spawnPosition, transform.rotation);
-				// does not work
-				left = true;
 			}
 			yield return new WaitForSeconds(cloudWait);
 		StartCoroutine ("cloudSpawnLeft");
 	}
-	void cloudSpawnRight(){
-		for(int i = 0; i < cloudCount; i++){
-			Vector2 spawnPosition = new Vector3 (Random.Range(-spawnValues.x, spawnValues.x), Random.Range (-spawnValues.y, spawnValues.y));
-			Instantiate (Cloud, spawnPosition, transform.rotation);
-			// does not work
-			right = true;
+	IEnumerator cloudSpawnBegin(){
+		for(int i = 0; i < cloudCountBegin; i++){
+			Vector2 spawnPosition = new Vector2 (Random.Range(-spawnValuesBegin.x, spawnValuesBegin.x), Random.Range (-spawnValuesBegin.y, spawnValuesBegin.y));
+			GameObject CloudBegin = (GameObject) Instantiate (Cloud, spawnPosition, transform.rotation);
 		}
+		yield return new WaitForSeconds(cloudWait);
 	}
-	
-
-
 }
