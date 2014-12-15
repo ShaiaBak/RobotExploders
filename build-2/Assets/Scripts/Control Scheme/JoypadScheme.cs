f5728b18bc8ee184b14476dd530a4ecf560e82ad
Shaia <shaia.bak@hotmail.com>
2014-12-14 17:18:10 -0800

    movment smoothing + control stuff + camera zoom

--- build-2/Assets/Scripts/Control Scheme/JoypadScheme.cs
+++ build-2/Assets/Scripts/Control Scheme/JoypadScheme.cs
@@ -19,13 +19,13 @@ public class JoypadScheme : ControlScheme {
 			
 		case 2:
 			// P2 keyboard controls
-			horizontal = "K_P2_Horizontal";
-			vertical = "K_P2_Vertical";
-			fireA = "K_Fire2a";
-			fireB = "K_Fire2b";
-			fireC = "K_Fire2c";
-			jump = "K_P2_Jump";
-			enter = "K_P2_Enter";
+			horizontal = "K_P1_Horizontal";
+			vertical = "K_P1_Vertical";
+			fireA = "K_Fire1a";
+			fireB = "K_Fire1b";
+			fireC = "K_Fire1c";
+			jump = "K_P1_Jump";
+			enter = "K_P1_Enter";
 			break;
 		}
 	}
